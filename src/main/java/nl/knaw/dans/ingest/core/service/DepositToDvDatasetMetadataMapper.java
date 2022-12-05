f0a511ef44d5473f8bb4bc408c0c052da1629b4f
Eric de Vries <hro.mail@gmail.com>
2022-12-05 17:28:59 +0100

    Cleanup and notes

--- src/main/java/nl/knaw/dans/ingest/core/service/DepositToDvDatasetMetadataMapper.java
+++ src/main/java/nl/knaw/dans/ingest/core/service/DepositToDvDatasetMetadataMapper.java
@@ -260,6 +260,8 @@ public class DepositToDvDatasetMetadataMapper {
         block.setDisplayName(displayName);
         block.setFields(result);
 
+        // TODO add de-duplication here
+
         fields.put(title, block);
     }
 

926480a6cee2be02b818f865bd237d7e08c47075
Eric de Vries <hro.mail@gmail.com>
2023-02-13 17:51:47 +0100

    Add a test for verifying paths

--- src/test/java/nl/knaw/dans/ingest/core/deposit/DepositReaderImplTest.java
+++ src/test/java/nl/knaw/dans/ingest/core/deposit/DepositReaderImplTest.java
@@ -16,7 +16,7 @@
 package nl.knaw.dans.ingest.core.deposit;
 
 import gov.loc.repository.bagit.domain.Bag;
-import gov.loc.repository.bagit.reader.BagReader;
+import gov.loc.repository.bagit.domain.Metadata;
 import nl.knaw.dans.ingest.core.io.BagDataManager;
 import nl.knaw.dans.ingest.core.io.FileService;
 import nl.knaw.dans.ingest.core.service.XmlReader;
@@ -31,6 +31,42 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class DepositReaderImplTest {
+
+    @Test
+    void readDeposit_should_call_with_correct_paths() throws Throwable {
+        var basePath = Path.of("/some/path/to/4e97185d-b38c-4ed9-bdf6-64339acfb6e8");
+        var xmlReader = Mockito.mock(XmlReader.class);
+        var fileService = Mockito.mock(FileService.class);
+
+        var bagDirResolver = Mockito.mock(BagDirResolver.class);
+        Mockito.doReturn(basePath.resolve("bagdir"))
+            .when(bagDirResolver).getValidBagDir(Mockito.any());
+
+        var bagDataManager = Mockito.mock(BagDataManager.class);
+        var config = new BaseConfiguration();
+        config.setProperty("dataverse.sword-token", "token");
+
+        var metadata = new Metadata();
+        metadata.add("Created", "2022-10-01T00:03:04+03:00");
+        var bag = new Bag();
+        bag.setMetadata(metadata);
+
+        Mockito.doReturn(bag)
+            .when(bagDataManager).readBag(Mockito.eq(basePath.resolve("bagdir")));
+
+        Mockito.doReturn(config)
+            .when(bagDataManager).readDepositProperties(Mockito.any());
+
+        var reader = new DepositReaderImpl(xmlReader, bagDirResolver, fileService, bagDataManager);
+        var result = reader.readDeposit(basePath);
+
+        // check if the bagDataManager is called with the correct paths
+        Mockito.verify(bagDataManager).readBag(Mockito.eq(basePath.resolve("bagdir")));
+        Mockito.verify(bagDataManager).readDepositProperties(Mockito.eq(basePath));
+
+        assertEquals("4e97185d-b38c-4ed9-bdf6-64339acfb6e8", result.getDepositId());
+    }
+
     @Test
     void mapToDeposit_should_return_new_deposit_for_sparse_data() {
         var xmlReader = Mockito.mock(XmlReader.class);
